c2ad308613c05c554d51ae6ed1e30fc68d9ae377
Vibhanshu Pandey <vibhanshu2207@gmail.com>
2025-01-13 18:45:17 +0530

    set user scalable to no

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,7 +15,7 @@ const RootLayout = ({ children }) => {
         <head>
           <meta
             name="viewport"
-            content="width=device-width, initial-scale=1.0, maximum-scale=3.0, minimum-scale=1.0 user-scalable=yes"
+            content="width=device-width, initial-scale=1.0, maximum-scale=3.0, minimum-scale=1.0 user-scalable=no"
           />
         </head>
         <body className="bg-offWhite overflow-x-hidden">
